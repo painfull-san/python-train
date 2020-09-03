ee5112a25e9a27a92d2223a681178674d55a9820
painfull-san <painfull@mail.ru>
2020-09-03 19:25:42 +0300

    Nothing changed

--- fibonacci.py
+++ fibonacci.py
@@ -11,7 +11,3 @@ def fib(n):
   print(a)
 
 fib(10)
-
-# Input: nums = [1,2,3,4]
-# Output: [1,3,6,10]
-# Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4]
--- leetcode.py
+++ leetcode.py
@@ -1,4 +1,12 @@
-
+#!/usr/bin/python3
 # Input: nums = [1,2,3,4]
 # Output: [1,3,6,10]
 # Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4]
+
+def nums(n):
+    result = []
+    s = 0
+    for item in n:
+        s += item
+        result.append(s)
+    return result
